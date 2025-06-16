397878c88f425a386a6a3ddd01d5b0b1cb91a4b2
Virtual <119105087+Virtually1@users.noreply.github.com>
2025-06-16 23:50:31 +0300

    transition add

--- Assets/Virtual_File/MovingProgram.cs
+++ Assets/Virtual_File/MovingProgram.cs
@@ -7,9 +7,6 @@ public class MovingProgram : MonoBehaviour
     public float xmove;
     public float ymove;
     public GameObject player;
-    public LayerMask walk;
-    public LayerMask players;
-    public bool walkable;
     public float speed=5f;
     public Rigidbody2D rb;
     void Start()
@@ -20,8 +17,7 @@ public class MovingProgram : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        walkable = Physics2D.OverlapBox(player.transform.position , new Vector2(0.5f, 0.5f), 0f, walk + ~players);
-        if (walkable)
+
             Direction();
     }
     public void Direction()
---
+++ Assets/Virtual_File/Teleport.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] teleportPoints;
+    public int zoneID=0;
+    public int idt = 0;
+    public Transitions trs;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trs.tpn)
+            player.transform.position = teleportPoints[idt].transform.position;
+    }
+    public void TeleportPlayer()
+    {
+        if (idt!=zoneID)
+        {
+           zoneID=idt;
+            trs.CrossFade();
+        }
+    }
+}
