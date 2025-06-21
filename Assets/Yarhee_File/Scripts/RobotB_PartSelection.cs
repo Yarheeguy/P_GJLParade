966b9bf3eef73a84b892f2aced20f757ddc5c4bb
Yarheeguy <125282780+Yarheeguy@users.noreply.github.com>
2025-06-21 22:06:59 +0100

    Parts are now added
    the parts has been added and a fully customizable robot is now functional, all that's left is the keywords to put in.

--- Assets/Yarhee_File/Scripts/RobotB_PartSelection.cs
+++ Assets/Yarhee_File/Scripts/RobotB_PartSelection.cs
@@ -8,17 +8,56 @@ public class RobotB_PartSelection : MonoBehaviour
 
     [SerializeField] GameObject eyesmenu;
 
+    [SerializeField] GameObject chestmenu;
+
+    [SerializeField] GameObject armsmenu;
+
+    [SerializeField] GameObject legsmenu;
+
 
     public void openHeadmenu()
     {
         headmenu.SetActive(true);
         eyesmenu.SetActive(false);
+        chestmenu.SetActive(false);
+        armsmenu.SetActive(false);
+        legsmenu.SetActive(false);
     }
 
     public void openEyesmenu()
     {
         headmenu.SetActive(false);
         eyesmenu.SetActive(true);
+        chestmenu.SetActive(false);
+        armsmenu.SetActive(false);
+        legsmenu.SetActive(false);
+    }
+
+    public void openChestmenu()
+    {
+        headmenu.SetActive(false);
+        eyesmenu.SetActive(false);
+        chestmenu.SetActive(true);
+        armsmenu.SetActive(false);
+        legsmenu.SetActive(false);
+    }
+
+    public void openArmsmenu()
+    {
+        headmenu.SetActive(false);
+        eyesmenu.SetActive(false);
+        chestmenu.SetActive(false);
+        armsmenu.SetActive(true);
+        legsmenu.SetActive(false);
+    }
+
+    public void openLegsmenu()
+    {
+        headmenu.SetActive(false);
+        eyesmenu.SetActive(false);
+        chestmenu.SetActive(false);
+        armsmenu.SetActive(false);
+        legsmenu.SetActive(true);
     }
 
 }
