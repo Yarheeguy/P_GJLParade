b46c4452d33e201abdcb1b72bdc7546ab7ad0cdb
Yarheeguy <125282780+Yarheeguy@users.noreply.github.com>
2025-06-22 10:44:16 +0100

    Put the peices together
    I put the peices together in the best of my abilities, everything else is up to Virtual.

--- Assets/Yarhee_File/Scripts/Feedback/FeedbackData.cs
+++ Assets/Yarhee_File/Scripts/Feedback/FeedbackData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FeedbackData : MonoBehaviour
 {
@@ -24,6 +25,11 @@ public class FeedbackData : MonoBehaviour
         if(StarRating < 0)
         {
             StarRating = 0;
+        }
+
+        if(StarRating >= 5)
+        {
+            SceneManager.LoadScene("WinningScene");
         }
     }
 }
